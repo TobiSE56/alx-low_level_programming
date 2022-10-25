5de103be1831599ffde2ede01b03841b7dee220c
TobiSE56 <m.juwon95@gmail.com>
2022-10-25 16:26:32 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+
+/**
+ * _putchar  -  write character to stdout
+ * @c: char to be printed
+ * Return: On success 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+
+}
